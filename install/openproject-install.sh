9037a2cdbe0d3babde8e3d19469b854cbfa0afed
Jason Walker <jcwalker3@yahoo.com>
2025-06-30 03:59:51 -0400

    Update OpenProject repo to 16.x

--- install/openproject-install.sh
+++ install/openproject-install.sh
@@ -21,7 +21,7 @@ msg_ok "Installed Dependencies"
 
 msg_info "Setting up OpenProject Repository"
 curl -fsSL "https://dl.packager.io/srv/opf/openproject/key" | gpg --dearmor >/etc/apt/trusted.gpg.d/packager-io.gpg
-curl -fsSL "https://dl.packager.io/srv/opf/openproject/stable/15/installer/debian/12.repo" -o "/etc/apt/sources.list.d/openproject.list"
+curl -fsSL "https://dl.packager.io/srv/opf/openproject/stable/16/installer/debian/12.repo" -o "/etc/apt/sources.list.d/openproject.list"
 msg_ok "Setup OpenProject Repository"
 
 msg_info "Setting up PostgreSQL Repository"
